6c0ad308a3fdcfc7eaa7be1262a0f795556031c8
sariDon <ritwickpal@gmail.com>
2015-08-19 17:11:45 +0530

    some_message

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>test php</title>
+  <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+echo 'ddd';
+?>
+</body>
+</html>
